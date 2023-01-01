f47d9db0f261787f328a7fa0afc6231fa8e5a43a
Menghui Nie <niemenghui@outlook.com>
2023-01-01 17:09:32 -0500

    before filter.

--- prj29bWebCoreMVC/Controllers/MoviesController.cs
+++ prj29bWebCoreMVC/Controllers/MoviesController.cs
@@ -20,10 +20,37 @@ namespace prj29bWebCoreMVC.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index99()
         {
               return View(await _context.Movie.ToListAsync());
         }
+
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
+        }
+
+
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
--- prj29bWebCoreMVC/Models/Movie.cs
+++ prj29bWebCoreMVC/Models/Movie.cs
@@ -12,6 +12,8 @@ namespace prj29bWebCoreMVC.Models
 		[DataType(DataType.Date)]
 		public DateTime ReleaseDate { get; set; }
 		public string? Genre { get; set; }
+
+		[Column(TypeName = "decimal(18, 2)")]
 		public decimal Price { get; set; }
 	}
 }
